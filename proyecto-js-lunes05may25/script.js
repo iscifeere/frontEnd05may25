af586d61ce64e820a2bcdc1009ee4a5c76d80025
Alfonso <alfonso.espinoza19@gmail.com>
2025-05-05 16:15:11 -0400

    first commit

---
+++ proyecto-js-lunes05may25/script.js
@@ -0,0 +1,36 @@
+const students = []
+
+const tableBody = document.querySelector("#studentsTable tbody")
+
+document.getElementById('studentForm').addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const name=document.getElementById('name').value.trim();
+    const lastName=document.getElementById('lastName').value.trim();
+    const grade=document.getElementById('grade').value.trim();
+
+    if(grade < 1 || grade > 7 || grade === null){
+        alert("Error Datos Incorrectos")
+        return
+    }
+
+    const student={name, lastName, grade};
+    students.push(student)
+
+    console.log(students)
+
+    this.reset()
+
+})
+
+function addStudentToTable(student)
+{
+    const row=document.createElement("tr")
+    row.innerHTML = 
+    `
+    <td>${student.name}</td>
+    <td>${student.lastName}</td>
+    <td>${student.grade.toFixed(1)}</td>
+    `;
+    tableBody.appendChild(row)
+}
