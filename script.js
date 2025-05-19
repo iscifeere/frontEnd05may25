d327d5bab7959d5cf22dce5f2ea996dbca68ca8d
Alfonso <alfonso.espinoza19@gmail.com>
2025-05-19 16:24:55 -0400

    agregado boton eliminar

--- script.js
+++ script.js
@@ -33,8 +33,23 @@ function addStudentToTable(student)
     <td>${student.name}</td>
     <td>${student.lastName}</td>
     <td>${student.grade}</td>
+    <td><button class="btn btn-danger delete">Eliminar</button></td>
     `;
+
+    row.querySelector(".delete").addEventListener("click",function(){
+        deleteEstudiante(student,row);
+    })
     tableBody.appendChild(row)
+}
+
+function deleteEstudiante(student,row){
+    // buscar el estudiante en el array
+    const index=students.indexOf(student);
+    if(index > -1){
+        students.splice(index,1);
+        row.remove();
+        calcularPromedio();
+    }
 }
 
 function calcularPromedio()
